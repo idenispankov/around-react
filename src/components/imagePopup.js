f47d3bb18c8b722376007a3c0a166907bbdfbc22
Denis Pankov <idenispankov@gmail.com>
2021-01-26 17:20:26 -0800

    image popup is displaying correctly now

--- src/components/imagePopup.js
+++ src/components/imagePopup.js
@@ -3,8 +3,8 @@ export default function imagePopup(props) {
     <div className={`modal modal_type_image ${props.card ? 'modal_is-open' : null}`}>
         <figure className="modal__figure">
           <button className="form__close-button form__close-button_type-image" onClick={props.onClose} aria-label="Close button" type="button"></button>
-          <img className="modal__image" src={props.link} alt={props.name}/>
-          <figcaption className="modal__image-title">{props.name}</figcaption>
+          <img className="modal__image" src={props.card && props.card.link} alt={props.card && props.card.name}/>
+          <figcaption className="modal__image-title">{props.card && props.card.name}</figcaption>
         </figure>
       </div>
   )
