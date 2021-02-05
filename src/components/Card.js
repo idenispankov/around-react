a5e659312b7f7918f85d5ee269bdcbdc781d03e6
Denis Pankov <idenispankov@gmail.com>
2021-02-04 17:30:33 -0800

    added likes and delete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,13 @@
+import { CurrentUserContext } from '../context/CurrentUserContext';
+import { useContext } from 'react';
+
 export default function Card({card, onCardClick}) {
+
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+
 
   function handleCardClick() {
     onCardClick(card);
@@ -6,12 +15,12 @@ export default function Card({card, onCardClick}) {
 
   return (
     <li className = "card">
-      <button className = "card__delete-button" aria-label = "Delete button" type = "button"/>
+      {isOwn && <button className = "card__delete-button" aria-label = "Delete button" type = "button"/>}
       <img className = "card__image" alt = {card.name} src = {card.link} onClick = {handleCardClick}/>
         <div className = "card__group">
           <h2 className = "card__text">{card.name}</h2> 
             <div className = "card__like-container">
-              <button className = "card__like-button" aria-label = "Like button" type = "button"/>
+              <button className = {`card__like-button ${isLiked ? 'card__like-button_active' : null}`} aria-label = "Like button" type = "button"/>
               <p className = "card__like-button-count">{card.likes.length}</p>
             </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,31 @@
 import {useState, useEffect} from 'react';
 import api from '../utils/api';
-import avatarDefault from '../images/avatar_type_dark.jpg';
 import Card from './Card';
+import { CurrentUserContext } from '../context/CurrentUserContext';
+import { useContext } from 'react';
 
 export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardClick}) {
-  const [userName, setUserName] = useState('');
-  const [userDescription, setUserDescription] = useState('');
-  const [userAvatar, setUserAvatar] = useState(avatarDefault);
   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    api.getUserInfo()
-      .then(res => {
-        setUserName(res.name)
-        setUserDescription(res.about)
-        setUserAvatar(res.avatar)
-      })
-      .catch(err => console.log(err))
-  }, [])
+  const currentUser = useContext(CurrentUserContext);
 
   useEffect(() => {
+    if(!currentUser) return;
     api.getCardList()
       .then(setCards)
-      .catch(err => console.log(err))
-  },[])
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currentUser]);
 
+
+//   function handleCardLike(card) {
+//     const isLiked = card.likes.some(i => i._id === currentUser._id);
+//     api.updateLikes(card._id, !isLiked).then((newCard) => {
+//       const newCards = cards.map((c) => c._id === card._id ? newCard : c);
+//       setCards(newCards);
+//     });
+// } 
 
 
   return (
@@ -32,12 +33,12 @@ export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardCli
     <main>
           <section className = "profile">
             <button className = "profile__avatar-edit" onClick = {onEditAvatar}>
-              <img className = "profile__avatar" src = {userAvatar} alt = "profile avatar"/>
+              <img className = "profile__avatar" src = {currentUser.avatar} alt = "profile avatar"/>
             </button>
             <div className = "profile__info">
-              <h1 className = "profile__text">{userName}</h1>
+              <h1 className = "profile__text">{currentUser.name}</h1>
               <button className = "profile__edit-button" onClick = {onEditProfile} aria-label = "Edit Avatar" type = "button"></button>
-              <p className = "profile__paragraph">{userDescription}</p>
+              <p className = "profile__paragraph">{currentUser.about}</p>
             </div>
             <button className = "profile__add-button" onClick = {onAddPlace} aria-label = "Add button" type = "button"></button>
           </section>
@@ -48,6 +49,7 @@ export default function Main({onEditAvatar, onEditProfile, onAddPlace, onCardCli
               <Card
                 card = {card}
                 key = {card._id}
+                currentUserId = {currentUser._id}
                 onCardClick = {onCardClick}
               />
               ))}
